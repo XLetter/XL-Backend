b0033b9543c1ab306f87e1a40f5bdb5f089d3898
Yoon-Suji <jidl88@ewhain.net>
2022-09-11 22:03:43 +0900

    feat: CORS 설정

---
+++ src/main/java/com/example/xlbackend/config/WebConfig.java
@@ -0,0 +1,15 @@
+package com.example.xlbackend.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("*")
+                .maxAge(3000);
+    }
+}
