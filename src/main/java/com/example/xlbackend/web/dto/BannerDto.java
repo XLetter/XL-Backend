5b17c65241a4a6e3c0908060ed85a13b1a28907b
Yoon-Suji <jidl88@ewhain.net>
2022-10-12 23:04:30 +0900

    fix: banner api

--- src/main/java/com/example/xlbackend/web/dto/BannerDto.java
+++ src/main/java/com/example/xlbackend/web/dto/BannerDto.java
@@ -9,11 +9,17 @@ public class BannerDto {
     private String title;
     private String bannerUrl;
     private String summary;
+    private Long webnovelId;
+    private String thumbnailUrl;
+    private String writerName;
 
     @Builder
     public BannerDto(Webnovel webnovel) {
         this.title = webnovel.getTitle();
         this.bannerUrl = webnovel.getBannerUrl();
         this.summary = webnovel.getSummary();
+        this.webnovelId = webnovel.getWebnovelId();
+        this.thumbnailUrl = webnovel.getThumbnailUrl();
+        this.writerName = webnovel.getWriter().getWriterName();
     }
 }
